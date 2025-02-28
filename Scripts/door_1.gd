caf05d90985bce609f57aff5ed67dc6e1717bdbf
VrushangP <vrushang.patel14@gmail.com>
2025-02-27 20:18:16 -0500

    fleshed out interactables

--- Scripts/door_1.gd
+++ Scripts/door_1.gd
@@ -1,11 +1,26 @@
 extends AnimatableBody2D
 
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var sprite = $Sprite2D
+@onready var speech_sound = preload("res://Assets/sounds/coin.wav")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
+const lines: Array[String] = [
+	"Hey there!"
+]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	print("Ready function called")
+	if interaction_area:
+		print("InteractionArea found")
+		interaction_area.interact = Callable(self, "_on_interact")
+		print("Callable assigned")
+	else:
+		print("ERROR: InteractionArea not found")
+
+func _on_interact():
+	print("Interaction triggered!")
+
+	#DialogManager.start_dialog(global_position, lines, speech_sound)
+	#sprite.flip_h = true if interaction_area.get_overlapping_bodies()[0].global_position.x < global_position.x else f 
+	#await DialogManager.dialog_finished
--- Scripts/interaction_manager.gd
+++ Scripts/interaction_manager.gd
@@ -35,7 +35,7 @@ func _sort_by_distance_to_player(area1, area2):
 
 func _input(event):
 	if event.is_action_pressed("interact") && can_interact:
-		if active_areas.size > 0:
+		if active_areas.size() > 0:
 			can_interact = false
 			label.hide()
 			
